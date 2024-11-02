b41cec4956b8892d780abf5496fadef016ee58e1
Danil Shipy4ka <105577586+Shipy4kaRU@users.noreply.github.com>
2024-11-02 17:28:06 +0400

    add build verion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { HashRouter as Router } from "react-router-dom/cjs/react-router-dom.min";
 
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
 );
